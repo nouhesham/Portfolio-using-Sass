baa90044357077a746c562df423c8b68bb83da1a
Nourhan <nourhanhesham263@gmail.com>
2023-04-28 22:33:00 +0300

    adding html text and css of the about and projects of the portfolio

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,26 @@
 "strict";
 const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItem = document.querySelectorAll(".menu-nav_item");
+
+let showMenu = false;
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItem.forEach((nav) => nav.classList.add("open"));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    showMenu = false;
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItem.forEach((nav) => nav.classList.remove("open"));
+  }
+}
+
+menuBtn.addEventListener("click", toggleMenu);
